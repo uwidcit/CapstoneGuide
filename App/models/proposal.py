abe91e5217ddb6cc20f0c7c22bd4dde59b8d7035
alexatwell <alexatwell246@gmail.com>
2023-05-14 21:24:40 -0400

    Push 1
    Created proposal (class, constructor)
    Edited user (role)

---
+++ App/models/proposal.py
@@ -0,0 +1,54 @@
+from App.database import db
+from datetime import datetime
+
+class Proposal(db.Model):
+    proposalId = db.Column(db.Integer, primary_key=True)
+    studentId = db.Column(db.Integer, db.ForeignKey('student.id'))
+    rubricId = db.Column(db.Integer, db.ForeignKey('rubric.id'))
+    problem_desc = db.Column(db.String)
+    solution_desc = db.Column(db.String)
+    notes = db.Column(db.String)
+    functionalities = db.Column(db.String)
+    technologies = db.Column(db.String)
+    num_members = db.Column(db.Integer)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def __init__(self, studentId, rubricId, problem_desc, solution_desc,
+                 num_members, notes, functionalities, technologies, created):
+        self.studentId = studentId
+        self.rubricId = rubricId
+        self.problem_desc = problem_desc
+        self.solution_desc = solution_desc
+        self.num_members = num_members
+        self.notes = notes
+        self.functionalities = functionalities
+        self.technologies = technologies
+        self.created = created
+
+# Sample Code
+    """
+    def __repr__(self):
+        return f'<listing {self.listingId} for ${self.price}>'
+
+    def toJSON_with_game(self):
+        return{
+            'listingId': self.listingId,
+            'ownerId': self.ownerId,
+            'condition': self.condition,
+            'price': self.price,
+            'status': self.status,
+            'created': self.created.strftime("%Y/%m/%d, %H:%M:%S"),
+            'game': self.game.toJSON(),
+        }
+
+    def toJSON(self):
+        return{
+            'title': self.game.title,
+            'owner': self.ownerId,
+            'condition': self.condition,
+            'price': self.price,
+            'created': self.created.strftime("%Y/%m/%d, %H:%M:%S"),
+            'status': self.status
+        }
+    """
--- App/models/user.py
+++ App/models/user.py
@@ -2,26 +2,33 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from App.database import db
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False, unique=True)
+    username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
+    user_role = db.Column(db.Boolean(False))
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, user_role):
         self.username = username
         self.set_password(password)
+        self.user_role = user_role
 
     def get_json(self):
-        return{
+        return {
             'id': self.id,
             'username': self.username
         }
 
     def set_password(self, password):
         """Create hashed password."""
-        self.password = generate_password_hash(password, method='sha256')
-    
+        self.password = generate_password_hash(password, method='scrypt`')
+
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
+
+    def set_supervisor(self):
+        self.user_role = True
+
 

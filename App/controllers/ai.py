b5ffaeea8d24f859f68e1fa634490b1b90d7c6d2
danteblunt <danteblunt810@gmail.com>
2023-06-20 12:54:50 -0400

    commented out AI tools

--- App/controllers/ai.py
+++ App/controllers/ai.py
@@ -1,12 +1,12 @@
-import openai
+# #import openai
 
-openai.api_key = "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"
+# openai.api_key = "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"
 
-def ai_test(query):
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
-   # print(response)
+# def ai_test(query):
+#     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
+#    # print(response)
 
-    f = open("App/tests/ai_ds.jsonl", "a")
-    f.write("file opened") #'{"prompt": "' + query + '", "completion": "' + str(response.choices[0].message.content) + '"}')
-    f.close()
-    return (str(response.choices[0].message.content))
+#     f = open("App/tests/ai_ds.jsonl", "a")
+#     f.write("file opened") #'{"prompt": "' + query + '", "completion": "' + str(response.choices[0].message.content) + '"}')
+#     f.close()
+#     return (str(response.choices[0].message.content))
--- App/tests/gpt_test.py
+++ App/tests/gpt_test.py
@@ -1,15 +1,15 @@
-import os
-import openai
+# import os
+# import openai
 
-query = input("Please enter query")
+# query = input("Please enter query")
 
-openai.api_key = "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"
+# openai.api_key = "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"
 
-openai.Completion.create(model="cap-mod", prompt=query)
+# openai.Completion.create(model="cap-mod", prompt=query)
 
-response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
-print(response)
+# response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
+# print(response)
 
-f = open("App/tests/ai_ds.jsonl", "a")
-f.write('{"prompt": "' + query + '", "completion": "' + str(response.choices[0].message.content) + '"}')
-f.close()
+# f = open("App/tests/ai_ds.jsonl", "a")
+# f.write('{"prompt": "' + query + '", "completion": "' + str(response.choices[0].message.content) + '"}')
+# f.close()
